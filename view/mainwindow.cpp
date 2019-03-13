89387ec708a83c55a1da1b4f534c64679251295b
arsonite <notarson@gmail.com>
2019-03-13 12:40:53 +0100

    added different style substances

--- view/mainwindow.cpp
+++ view/mainwindow.cpp
@@ -2,31 +2,24 @@
 #include "ui_mainwindow.h"
 
 #include <QStandardPaths>
-
 MainWindow::MainWindow(QWidget *parent):
     QMainWindow(parent),
     ui_(new Ui::MainWindow),
     lg(Logger("MainWindow"))
 {
-
-    const int width = size().width();
-    const int height = size().height();
-
     setWindowTitle("synthia");
-    //setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 
     ui_->setupUi(this);
+
     ui_->view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     ui_->view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    ui_->view->resize(width, height);
-    ui_->view->setSceneRect(0, 0, width, height);
     ui_->view->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    ui_->view->setStyleSheet(QString("border: none; background-color: #%1;").arg("353535"));
 
-    QGraphicsScene *scene = new QGraphicsScene();
-    scene->setBackgroundBrush(QBrush(QColor(35, 35, 35)));
-    ui_->view->setScene(scene);
+    ui_->central->setStyleSheet(QString("background-color: #%1;").arg("353535"));
 
-    QWidget *titleBar = new QWidget(this);
+    ui_->menubar->setStyleSheet(QString("border-bottom: 2px solid #222; color: #fff; background-color: #%1;").arg("353535"));
 
     lg.log("Started up MainWindow with Application-Path: " + QString(getenv("APPDATA")));
 }
